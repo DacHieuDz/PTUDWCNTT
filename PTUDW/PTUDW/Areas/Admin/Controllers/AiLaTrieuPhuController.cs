706aa0155dab7da5d3464aa0e18c2003e79114ba
DacHieuDz <116139910+DacHieuDz@users.noreply.github.com>
2023-12-03 21:42:20 +0700

    Upload Finish

---
+++ PTUDW/PTUDW/Areas/Admin/Controllers/AiLaTrieuPhuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTUDW.Areas.Admin.Controllers
+{
+    public class AiLaTrieuPhuController : Controller
+    {
+        // GET: Admin/AiLaTrieuPhu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HowToPlay()
+        {
+            return View();
+        }
+        public ActionResult PlayGame()
+        {
+            return View();
+        }
+    }
+}
